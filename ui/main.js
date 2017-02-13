177daccc73f3d5bddf15916473e0c47f429f65e5
abhayjha062 <abhayjha062@gmail.com>
2017-02-13 18:22:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,18 @@ button.onclick = function () {
 request.open('GET','http://abhayjha062.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var sumit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //should make a request to server and send the name
+    //capture a list of name and render it as a list
+    var names =['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i< name.length; i++) {
+        list += '<li>' + names[i] + '<li>';
+    }
+    var ul =document.getElementById('namelist');
+    ul.innerHTML = list;
+};
