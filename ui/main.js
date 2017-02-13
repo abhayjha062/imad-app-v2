854e9cc363c89249a239b23c10518e3fc8952c25
abhayjha062 <abhayjha062@gmail.com>
2017-02-13 19:23:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,7 @@ submit.onclick = function() {
     //Not done yet
 };
 //Make a Request
-request.open('GET','http://abhayjha062.imad.hasura-app.io/submit-name?name'+ name, true);
+request.open('GET','http://abhayjha062.imad.hasura-app.io/submit-name?name='+ name, true);
 request.send(null);
 };
    
